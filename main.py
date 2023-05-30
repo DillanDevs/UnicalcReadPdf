a14c35e63d1767ca7c14aa126053112501e84711
Dillan <dillansdev@gmail.com>
2023-05-29 19:55:15 -0500

    Se añadieron medidas de seguridad, asi como permiso para el uso de la api

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import os
 import PyPDF2
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 from reader import reader
 
 app = FastAPI()
+
+# Configurar los orígenes permitidos
+origins = [
+    "http://localhost:3000",  # Agrega aquí el origen de tu aplicación de React
+    # Otros orígenes permitidos si es necesario
+]
+
+# Habilitar CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/upload/pdf")
 async def process_pdf(file: UploadFile = File(...)):
